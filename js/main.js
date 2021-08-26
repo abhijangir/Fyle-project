a81b2f7f532bb65e0329db5b2a346dd9818af004
Abhi <abhijangir124@gmail.com>
2021-08-26 09:17:52 +0530

    new create file

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function before(){
+  document.getElementById('myImage').src="images/food.jpg";
+}
+
+function after(){
+  document.getElementById('myImage').src="images/lief.jpg";
+}
+
+function present(){
+  document.getElementById('myImage').src="images/image.png";
+}
